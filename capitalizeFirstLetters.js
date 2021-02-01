72009679b3257b76b122f771b62a66e7050674f3
baptiste_gfy <geffroy.baptiste@outlook.fr>
2021-02-01 22:30:26 +0100

    git tag step-1

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -2,12 +2,24 @@
 const assert = require("assert");
 
 // WRITE THE ACTUAL FUNCTION HERE
-
-//  verifie que c'est bien une fonction
+function capitalizeFirstLetters(input) {
+  const result =
+    input.length > 0
+      ? input
+          .split(" ")
+          .map((e) => e[0].toUpperCase() + e.slice(1))
+          .join(" ")
+      : "";
+  return result;
+}
+// Check that capitalizeFirstLetters is a function
 assert.strictEqual(typeof capitalizeFirstLetters, "function");
-// verifie que cette fonction a bien 1 parametre
+
+// Check that capitalizeFirstLetters accepts one argument
 assert.strictEqual(capitalizeFirstLetters.length, 1);
-// verifie le fonctionnement de la fonction
+
+// Check that capitalizeFirstLetters transforms correctly
 assert.strictEqual(capitalizeFirstLetters("je suis dev"), "Je Suis Dev");
 assert.strictEqual(capitalizeFirstLetters("baptiste"), "Baptiste");
 assert.strictEqual(capitalizeFirstLetters(""), "");
+console.log("OK");
